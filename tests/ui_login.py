f5f6502737766239302fd271ce1e1c98d809c7f5
Paddy Harker <Paddy@gmail.com>
2024-06-13 16:40:51 +0000

    testing

--- tests/ui_login.py
+++ tests/ui_login.py
@@ -2,7 +2,6 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 class UITest(unittest.TestCase):
@@ -10,6 +9,7 @@ class UITest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         options = Options()
+        options.binary_location = "google-chrome/opt/google/chrome/"  # Specify the path to the Chrome binary
         options.headless = True
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
@@ -36,20 +36,6 @@ class UITest(unittest.TestCase):
         login_button.click()
 
         self.assertTrue(self.driver.find_element(By.ID, "logout").is_displayed())
-
-    # def test_register(self):
-    #     self.driver.get("http://localhost:5000/auth/register")
-    #     username_field = self.driver.find_element(By.NAME, "username")
-    #     password_field = self.driver.find_element(By.NAME, "password")
-    #     confirm_field = self.driver.find_element(By.NAME, "confirm")
-    #     login_button = self.driver.find_element(By.NAME, "submit")
-
-    #     username_field.send_keys("testuser")
-    #     password_field.send_keys("password")
-    #     confirm_field.send_keys("password")
-    #     login_button.click()
-
-        # self.assertTrue(self.driver.find_element(By.ID, "logout").is_displayed())
 
 if __name__ == "__main__":
     unittest.main()
